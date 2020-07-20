112f3e9fc6442c9f05cd19a0e3d5466fc3ad28d0
soulsystem00 <63180108+soulsystem00@users.noreply.github.com>
2020-07-20 18:20:14 +0900

    초기 커밋.

---
+++ thunder_distance_cal/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace thunder_distance_cal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_end = new System.Windows.Forms.Button();
+            this.txt_sec = new System.Windows.Forms.TextBox();
+            this.txt_dis = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(223, 70);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_end
+            // 
+            this.btn_end.Location = new System.Drawing.Point(346, 70);
+            this.btn_end.Name = "btn_end";
+            this.btn_end.Size = new System.Drawing.Size(75, 23);
+            this.btn_end.TabIndex = 1;
+            this.btn_end.Text = "end";
+            this.btn_end.UseVisualStyleBackColor = true;
+            this.btn_end.Click += new System.EventHandler(this.btn_end_Click);
+            // 
+            // txt_sec
+            // 
+            this.txt_sec.Location = new System.Drawing.Point(210, 148);
+            this.txt_sec.Name = "txt_sec";
+            this.txt_sec.Size = new System.Drawing.Size(100, 21);
+            this.txt_sec.TabIndex = 2;
+            // 
+            // txt_dis
+            // 
+            this.txt_dis.Location = new System.Drawing.Point(209, 213);
+            this.txt_dis.Name = "txt_dis";
+            this.txt_dis.Size = new System.Drawing.Size(100, 21);
+            this.txt_dis.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_dis);
+            this.Controls.Add(this.txt_sec);
+            this.Controls.Add(this.btn_end);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_end;
+        private System.Windows.Forms.TextBox txt_sec;
+        private System.Windows.Forms.TextBox txt_dis;
+    }
+}
+
---
+++ thunder_distance_cal/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace thunder_distance_cal
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch Stopwatch;
+        Thread thread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void counter()
+        {
+            while(true)
+            {
+                TimeSpan ts = Stopwatch.Elapsed;
+                double sec = ts.TotalSeconds;
+                txt_sec.Text = sec.ToString("F2");
+            }
+            
+        }
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            
+            Stopwatch = new Stopwatch();
+            thread = new Thread(counter);
+            Stopwatch.Start();
+            thread.Start();
+        }
+
+        private void btn_end_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+            Stopwatch.Stop();
+
+            TimeSpan timeSpan = Stopwatch.Elapsed;
+            double sec = timeSpan.TotalSeconds;
+            txt_sec.Text = sec.ToString("F2");
+            double distance = 340 * sec;
+            txt_dis.Text = distance.ToString("F2");
+
+        }
+    }
+}
